36284e1a5a0a83fa8e8b1d1f5535608a6d939980
RAVI KUMAR <p.ravikumar95333@gmail.com>
2023-01-21 01:47:55 +0530

    My commit message

---
+++ f1.js
@@ -0,0 +1,24 @@
+console.log("hiii");
+console.log(123);
+console.table({a:1,b:2,c:3});
+let js ="amazing"
+console.log(js);
+console.log(2+3+56);
+//values and variables
+//variable name should be allow letters, $,_
+let firstName="ravi";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+ //variable name conventions
+ let first_name="ravi";
+ let $function="js";
+
+ //data types
+ //premitive num,boolean, string, undefine, null, bigint
+ let javascriptFun=false;
+ console.log(javascriptFun);
+ console.log(typeof javascriptFun);
